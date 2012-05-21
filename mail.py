eb0646cdee016b31c220596b9e70ceb5f63b1bf8
Antti Jaakkola <annttu@kapsi.fi>
2012-05-21 16:07:26 +0300

    class is better than plain function

--- mail.py
+++ mail.py
@@ -7,7 +7,7 @@
 # Author: Antti Jaakkola <annttu(a)removethis.annttu.fi>
 #         Kapsi Internet-käyttäjät ry 2012
 #
-# This code is released under the public domain without any 
+# This code is released under the public domain without any
 # warranty! Use as you wish.
 
 import textwrap
@@ -17,78 +17,110 @@ import time
 from email.Header import Header
 from email.mime.text import MIMEText
 
-smtpserver = 'mail.domain.tld'
-# if your SMTP server requires auth fill smtpuser and smtpauth
-# else leave empty
-smtpuser = ''
-smtppass = ''
-
 sender = 'My name <me@domain.tld>'
 
-## header encoding
-header_charset = "iso-8859-1"
-
-def makeMessage(message, subject, to, sender, encoding, charset):
-    msg = MIMEText(message.decode(encoding), _subtype='plain',
-                    _charset=charset)
-    msg['Date'] = email.utils.formatdate(localtime=time.localtime(
-                                        float(time.strftime("%s"))))
-    # headers are always encoded as header_charset
-    msg['Subject'] = Header(unicode(subject, encoding), header_charset)
-    sender_name, sender_addr = email.utils.parseaddr(sender)
-    sender_name = str(Header(unicode(sender_name), header_charset))
-    msg['From'] =  email.utils.formataddr((sender_name,
-                                            sender_addr.encode('ascii')))
-    recipient_name, recipient_addr = email.utils.parseaddr(to)
-    recipient_name = str(Header(unicode(recipient_name), header_charset))
-    msg['To'] = email.utils.formataddr((recipient_name, 
-                                        recipient_addr.encode('ascii')))
-    msg = msg.as_string()
-    return str(msg)
-
-def sendmail(message, subject, recipients=[], sender=sender,
-             encoding="utf-8", charset="iso-8859-1"):
-    """Simple function to send emails easily
-    encoding: input string encoding
-    charset: output message body charset
-    recipient: list of recipient address, formatted as 
-            realname <email>, 
-            email,
-            [realname, email] or
-            (realname, email)
+class Mailer():
+    """if your SMTP server requires auth fill smtpuser and smtpauth
+       else leave them empty
     """
-
-    if len(recipients) == 0:
-        return False
-    if len(message) == 0:
-        return False
-    message = message.replace("—", " - ")
-    message = message.replace("”", "\"")
-    message = '\n'.join(textwrap.wrap(message,72))
-    session = smtplib.SMTP(smtpserver)
-    if smtpuser is not '':
-        session.login(smtpuser, smtppass)
-    error = ""
-    for recipient in recipients:
-        if type(recipient) == type([]) or type(recipient) == type(('','')):
-            mailaddr = recipient[1]
-            recipient = email.utils.formataddr(recipient)
+    def __init__(self, message, subject, recipients=[], sender=sender,
+                encoding="utf-8", charset="iso-8859-1",
+                smtpserver=None, smtpuser = None, smtppass = None):
+        if not smtpserver:
+            self.smtpserver = 'localhost'
         else:
-            realname, mailaddr = email.utils.parseaddr(recipient)
+            self.smtpserver = smtpserver
+        self.smtpuser = smtpuser
+        self.smtppass = smtppass
+        self.sender = sender
+        self.subject = subject
+        self.message = message
+        self.encoding = encoding
+        self.charset = charset
+        self.recipients = recipients
+        self.session = None
+        ## header encoding
+        self.header_charset = "iso-8859-1"
 
-        smtpresult = session.sendmail(sender, [mailaddr], 
-                                makeMessage(message, subject, recipient, 
-                                sender, encoding, charset))
-        if smtpresult:
-            for recip in smtpresult.keys():
-                error += "Couldn't delivery mail to: %s Error: %s\n" % (
-                        recip, smtpresult[recip][0], smtpresult[recip][1])
-        else:
-            print("Message sent successfully to %s" % recipient)
 
-    session.quit()
-    if error != "":
-        raise smtplib.SMTPException, error
+
+    def makeMessage(self, to, message=None, subject=None):
+        if not message:
+            message = self.message
+        if not subject:
+            subject = self.subject
+        msg = MIMEText(message.decode(self.encoding), _subtype='plain',
+                        _charset=self.charset)
+        msg['Date'] = email.utils.formatdate(localtime=time.localtime(
+                                            float(time.strftime("%s"))))
+        # headers are always encoded as header_charset
+        msg['Subject'] = Header(unicode(subject, self.encoding),
+                                                    self.header_charset)
+        sender_name, sender_addr = email.utils.parseaddr(self.sender)
+        sender_name = str(Header(unicode(sender_name, self.encoding),
+                                                   self.header_charset))
+        msg['From'] =  email.utils.formataddr((sender_name,
+                                               sender_addr.encode('ascii')))
+        recipient_name, recipient_addr = email.utils.parseaddr(to)
+        recipient_name = str(Header(unicode(recipient_name, self.encoding),
+                                                       self.header_charset))
+        msg['To'] = email.utils.formataddr((recipient_name,
+                                            recipient_addr.encode('ascii')))
+        msg = msg.as_string()
+        return str(msg)
+
+    def sendmail(self, message=None, subject=None, recipients=None):
+        """Simple function to send emails easily
+        encoding: input string encoding
+        charset: output message body charset
+        recipient: list of recipient address, formatted as
+                realname <email>,
+                email,
+                [realname, email] or
+                (realname, email)
+        """
+
+        if not recipients:
+            recipients = self.recipients
+        if len(recipients) == 0:
+            return False
+        if not message:
+            message = self.message
+        if len(message) == 0:
+            return False
+        if not subject:
+            subject = self.subject
+        message = message.replace("—", " - ")
+        message = message.replace("”", "\"")
+        message = '\n'.join(textwrap.wrap(message,72))
+        if not self.session:
+            self.session = smtplib.SMTP(self.smtpserver)
+            if self.smtpuser:
+                self.session.login(self.smtpuser, self.smtppass)
+        error = ""
+        for recipient in recipients:
+            if type(recipient) == type([]) or type(recipient) == type(
+                                                                   ('','')):
+                mailaddr = recipient[1]
+                recipient = email.utils.formataddr(recipient)
+            else:
+                realname, mailaddr = email.utils.parseaddr(recipient)
+
+            smtpresult = self.session.sendmail(self.sender, [mailaddr],
+                                  self.makeMessage(recipient, message,
+                                                                subject))
+            if smtpresult:
+                for recip in smtpresult.keys():
+                    error += "Couldn't delivery mail to: %s Error: %s\n" % (
+                          recip, smtpresult[recip][0], smtpresult[recip][1])
+            else:
+                print("Message sent successfully to %s" % recipient)
+
+        if error != "":
+            raise smtplib.SMTPException, error
+
+    def __del__(self):
+        self.session.quit()
 
 
 if __name__ == "__main__":
@@ -100,5 +132,10 @@ if __name__ == "__main__":
 '''
 
     # send mail
-    recipients = ['me@somedomain.tld', ['Another me', 'me@otherdomain.tld']]
-    sendmail(msg, 'Spam, delete me!!', recipients=recipients)
+    recipients = [
+        'me@somedomain.tld',
+        ['Another me', 'me@otherdomain.tld']
+    ]
+    mailer = Mailer(msg, 'Spam, delete me!!', recipients=recipients,
+    sender='me@myself.tld')
+    mailer.sendmail()
